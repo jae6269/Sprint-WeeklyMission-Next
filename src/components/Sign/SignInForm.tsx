18adca5f36144258a46da154de1db69c26059fe1
jae6269 <jae6269@gmail.com>
2024-03-30 16:19:59 +0900

    style: SignInForm placeholder 및 focus border설정

--- src/components/Sign/SignInForm.tsx
+++ src/components/Sign/SignInForm.tsx
@@ -1,42 +1,51 @@
 import { useForm } from 'react-hook-form';
+import Image from 'next/image';
 import styles from '@/src/styles/signInForm.module.css';
-import { SIGN_IN } from '@/src/constants/signConstants';
+import {
+  SIGN_IN,
+  EMAIL_PLACEHOLDER,
+  PW_PLACEHOLDER,
+} from '@/src/constants/signConstants';
 import SignHeader from './SignHeader';
 
+interface FormType {
+  email: string;
+  password: string;
+}
+
 export default function SignInForm() {
+  const { register, handleSubmit } = useForm({ mode: 'onBlur' });
+
   return (
     <div className={styles.signBackground}>
       <div className={styles.formContainer}>
         <SignHeader type={SIGN_IN} />
-        <form className={styles.signForm} id="sign-form">
+        <form
+          className={styles.signForm}
+          onSubmit={handleSubmit((data) => alert(JSON.stringify(data)))}
+        >
           <div className={styles.email}>
             <label className={styles.signLabel} htmlFor="signin-email">
               이메일
             </label>
             <input
-              className={styles.signInput}
+              id="email"
               type="email"
-              name="signin-email"
-              id="signin-email"
+              className={styles.signInput}
+              placeholder={EMAIL_PLACEHOLDER}
+              {...register('email')}
             />
-            <div className="email__error-msg" id="email__error-msg"></div>
           </div>
           <div className={styles.password}>
             <label className={styles.signLabel} htmlFor="signin-pw">
               비밀번호
             </label>
             <input
-              className={styles.signInput}
+              id="password"
               type="password"
-              name="signin-pw"
-              id="signin-pw"
-            />
-            <div className="pw__error-msg" id="pw__error-msg"></div>
-            <img
-              src="./icons/eyesoff.png"
-              alt="eyesoff"
-              className="pw-onoff"
-              id="pw-onoff"
+              className={styles.signInput}
+              placeholder={PW_PLACEHOLDER}
+              {...register('password')}
             />
           </div>
           <button className={styles.button} id="signin-button">
--- src/constants/signConstants.ts
+++ src/constants/signConstants.ts
@@ -1,2 +1,5 @@
 export const SIGN_IN = 'sign_in';
 export const SIGN_UP = 'sign_up';
+export const EMAIL_PLACEHOLDER = 'Enter email address';
+export const PW_PLACEHOLDER = 'Enter password';
+export const PW_REPEAT_PLACEHOLDER = 'Enter password again';
